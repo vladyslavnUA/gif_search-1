97f902baafcdf7231d6ca8b4f08a449ded2ee2db
Vladyslav Nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2019-09-25 03:57:51 -0700

    updating app.py -> added functions, variables

--- app.py
+++ app.py
@@ -7,20 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    search = request.args.get('search')
-
-    print(search)
-
-
+    query = request.args.get('query')
+    
     # TODO: Extract the query term from url using request.args.get() √
 
-
+    query = request.args.get('query')
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
+    # 'params' is defining the parameter values for each variable
+    params = {
+        
+        "s": search,
 
+        "apikey" = "LIVDSRZULELA",  # test value
+
+        "lmt" = 10,   
+    }
 
 
 
@@ -28,52 +33,49 @@ def index():
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
+    r = requests.get("https://api.tenor.com/v1/search", params = params)
 
-# set the apikey and limit
-    apikey = "LIVDSRZULELA"  # test value
-    lmt = 10
 
-    # our test search
-    search_term = search
-
-    # get the top 8 GIFs for the search term
-    r = requests.get(
-        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
-
+    # use '.json()' function to get the JSON of the return function
     if r.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
+    # load the GIFs using the urls for the smaller GIF sizes
         top_10gifs = json.loads(r.content)
-        print(top_10gifs)
+        #print(top_10gifs)
+        top_9gifs = top_9gifs["results"]
     else:
         top_10gifs = None
-
-
-# continue a similar pattern until the user makes a selection or starts a new search.
-
-
-    # TODO: Use the '.json()' function to get the JSON of the returned response
-    # object
-
-
-
-
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
-    # which contains the GIFs as a list
-    gif_list = top_10gifs["results"]
-
-
 
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
 
+    return render_template("index.html", gifs = top_10gifs)
 
+@app.route("/search-result")
+def search_results():
+    query = request.args.get('search')
+    # set the apikey and limit
+    search = {
+        "s": search,
 
+        "apikey" = "LIVDSRZULELA",  # test value
 
+        "lmt" = 10,
+    }
 
-    return render_template("index.html",
-    gif_list = gif_list)
+    # get the top 8 GIFs for the search term
+    r = requests.get("https://api.tenor.com/v1/search", params = search)
+    #("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s(search_term, apikey, lmt))"
+
+    if r.status_code == 200:
+        searchResult = json.loads(r.content)
+        searchResult = searchResult["results"]
+    else:
+        searchResult = None
+    
+    return render_template("index.html", gifs = searchResult)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
